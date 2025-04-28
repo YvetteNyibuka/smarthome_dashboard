f2e6e52214b523baa857af7e85a8b968bc8c3689
niroberto <robertwilly668@gmail.com>
2025-04-29 01:01:05 +0200

    fix/ deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 // const withNextIntl = require("next-intl/plugin")();
-const withAntdLess = require("next-plugin-antd-less");
+// const withAntdLess = require("next-plugin-antd-less");
 const nextConfig = withNextIntl(
   withAntdLess({
     reactStrictMode: true,
@@ -30,6 +30,10 @@ const nextConfig = withNextIntl(
 
 module.exports = nextConfig;
 function withNextIntl(arg0: any) {
+  throw new Error("Function not implemented.");
+}
+
+function withAntdLess(arg0: { reactStrictMode: boolean; typescript: { ignoreBuildErrors: boolean; }; eslint: { ignoreDuringBuilds: boolean; }; experimental: { forceSwcTransforms: boolean; missingSuspenseWithCSRBailout: boolean; }; webpack: (config: { resolve: any; }) => { resolve: any; }; }): any {
   throw new Error("Function not implemented.");
 }
 
