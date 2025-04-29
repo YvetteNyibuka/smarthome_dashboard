56546e4d58dd56129a9412f4dbec28d48897bf21
niroberto <robertwilly668@gmail.com>
2025-04-29 09:37:05 +0200

    fix/ deployment

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,7 @@
-/** @type {import('next').NextConfig} */
-// const withNextIntl = require("next-intl/plugin")();
-// const withAntdLess = require("next-plugin-antd-less");
-const nextConfig = withNextIntl(
-  withAntdLess({
-    reactStrictMode: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    experimental: {
-      forceSwcTransforms: true,
-      missingSuspenseWithCSRBailout: false,
-    },
-    webpack: (config: { resolve: any; }) => {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          fs: false,
-          path: false,
-          os: false,
-        },
-      };
-      return config;
-    },
-  })
-);
+import type { NextConfig } from "next";
 
-module.exports = nextConfig;
-function withNextIntl(arg0: any) {
-  throw new Error("Function not implemented.");
-}
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
-function withAntdLess(arg0: { reactStrictMode: boolean; typescript: { ignoreBuildErrors: boolean; }; eslint: { ignoreDuringBuilds: boolean; }; experimental: { forceSwcTransforms: boolean; missingSuspenseWithCSRBailout: boolean; }; webpack: (config: { resolve: any; }) => { resolve: any; }; }): any {
-  throw new Error("Function not implemented.");
-}
-
+export default nextConfig;
